576bef854ac028940e16a58d917a1003a4248387
unclejimbo <1440219835@qq.com>
2015-04-28 18:30:22 +0800

    extract mapcluster into a seperate file and take geojson as input

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -3,79 +3,6 @@ var HALF_MERC = 20037508.34;
 var PI = 245850922 / 78256779;
 var MAX_ZOOM = 20;
 
-// cluster is based on this distinctive score
-// feature should be an array of GeoJSON feature objects
-function dScore(features, level, extent) {
-    ++level;
-    var xOffset = [       0,   MERC/3, 2*MERC/3,
-                   2*MERC/3,   MERC/3,        0,
-                          0,   MERC/3, 2*MERC/3];
-    var yOffset = [       0,        0,        0,
-                     MERC/3,   MERC/3,   MERC/3,
-                   2*MERC/3, 2*MERC/3, 2*MERC/3];
-    for (var index = 0; index < 9; ++index) {
-        var left = extent[0] + xOffset[index];
-        var bottom = extent[1] + yOffset[index];
-        var right = extent[2] + xOffset[index];
-        var top = extent[3] + yOffset[index];
-        var gridLen = MERC/(Math.pow(2,level));
-        // x, y are grid coords
-        var x0 = Math.floor((left+HALF_MERC)/gridLen) - 1;
-        var x1 = Math.floor((right+HALF_MERC)/gridLen) + 1;
-        var y0 = Math.floor((bottom+HALF_MERC)/gridLen) - 1;
-        var y1 = Math.floor((top+HALF_MERC)/gridLen) + 1;
-        var vipArr = new Array(x1-x0+1);
-        for (var i = 0; i < vipArr.length; ++i) {
-            vipArr[i] = new Array(y1-y0+1);
-            for (var j = 0; j < vipArr[i].length; ++j) {
-                vipArr[i][j] = -1;
-            }
-        }
-        for (var i = 0; i < features.length; ++i) {
-            var x = 0, y = 0;
-            var xt, yt;
-            for (var j = 0; j <= level; ++j) {
-                switch (features[i].properties.mCode[index][j]) {
-                    case 0: x += Math.pow(2, level-j)*0;
-                            y += Math.pow(2, level-j)*0; break;
-                    case 1: x += Math.pow(2, level-j)*1;
-                            y += Math.pow(2, level-j)*0; break;
-                    case 2: x += Math.pow(2, level-j)*0;
-                            y += Math.pow(2, level-j)*1; break;
-                    case 3: x += Math.pow(2, level-j)*1;
-                            y += Math.pow(2, level-j)*1; break;
-                }
-            }
-            //x < x0 ? xt = x - x0 + Math.pow(2, level+1) :
-                     xt = x - x0;
-            // y < y0 ? yt = y - y0 + Math.pow(2, level+1) :
-                     yt = y - y0;
-            var feature = new ol.format.GeoJSON().readFeature(features[i], {
-                dataProjection: 'EPSG:4326',
-                featureProjection: 'EPSG:900913'
-            });
-            var coord = feature.getGeometry().getCoordinates();
-            if (features[i].properties.impScore > vipArr[xt][yt]) 
-                vipArr[xt][yt] = features[i];
-        }
-        for (var i = 0; i < vipArr.length; ++i) {
-            for (var j = 0; j < vipArr[i].length; ++j) {
-                if (vipArr[i][j] != -1) 
-                    ++vipArr[i][j].properties.dScore[level];
-            }
-        }
-    }
-}
-
-function quadTreeCluster(features, level, DS) {
-    var clustered = new Array();
-    for (var i = 0; i < features.length; ++i) {
-        if (features[i].getProperties().dScore[level+1] >= DS)
-            clustered.push(features[i]);
-    }
-    return clustered;
-}
-
 var map = new ol.Map({
     view: new ol.View({
         center: [0, 0],
@@ -113,8 +40,7 @@ app.controller('mapCtrl', function($http) {
                                   ymin: mapExtent[1] - gridLen,
                                   xmax: mapExtent[2] + gridLen,
                                   ymax: mapExtent[3] + gridLen}});
-    promise.success(function(JSONs) {
-        dScore(JSONs, zoom+2, mapExtent);
+    promise.success(function(JSONs) {       
         var originSource = new ol.source.Vector();
         var originFeatures = new Array();
         JSONs.forEach(function(fj) {
@@ -131,14 +57,41 @@ app.controller('mapCtrl', function($http) {
         });
         map.addLayer(originLayer);
 
+         var fill = new ol.style.Fill({
+           color: [255,0,0,0.3]
+         });
+         var stroke = new ol.style.Stroke({
+           color: [255.0,0,1],
+           width: 1.25
+         });
+        var img = new ol.style.Icon({
+            src: '/images/1.jpg',
+            size: [40,40]
+        });
+         var styles = [
+           new ol.style.Style({
+             image: img,
+             fill: fill,
+             stroke: stroke
+           })
+         ];
+        var bigJSONs = mapcluster(JSONs, mapExtent, zoom);
+        var bigFeatures = new Array();
+        bigJSONs.forEach(function(bj) {
+            var bigFeature = new ol.format.GeoJSON().readFeature(bj, {
+                dataProjection: 'EPSG:4326',
+                featureProjection: 'EPSG:900913'
+            });
+            bigFeatures.push(bigFeature);
+        });
         var bigSource = new ol.source.Vector({
             projection: 'EPSG:900913'
         });
-        var bigFeatures = quadTreeCluster(originFeatures, zoom+2, 9);
         bigSource.addFeatures(bigFeatures);
         var bigLayer = new ol.layer.Vector({
             source: bigSource,
-            title: 'Big Layer'
+            title: 'Big Layer',
+            style: styles[0]
         });
         map.addLayer(bigLayer);
     });
---
+++ public/javascripts/mapcluster.js
@@ -0,0 +1,114 @@
+/*
+  Cluster an array of GeoJSON Features or Feature Collection and gives
+  none-overlapping results for rendering on a map.
+  
+  Parameters:
+    geoJSONS   <Array(GeoJSON Features) | GeoJSON Feature Collection>
+    extent     <Array(Number)> in forms of [minx, miny, max, maxy]
+    zoom       <Number> the current zoom of the map
+    (opt)size  <Number> size of the img in pixels, default 40px
+    (opt)DS    <Number> distinctive score, ranging [0, 9], default 9
+
+  Returns:
+    <Array(GeoJSON Features) | GeoJSON Feature Collection> same as input
+
+  Notes:
+    1. Make sure you have "mCode" and "impScore" in field "properties".
+       The field "mCode" should hold morton code computed in advance,
+       and the field "impScore" specifies a point's value in clustering.
+    2. Using extent to narrow down the area in which you want to cluster,
+       by default it should be extent of the viewport.
+    3. Set DS to 9 makes sure there's no overlap. Less DS value will return
+       more points but potentially overlapping.
+ */
+function mapcluster(geoJSONs, extent, zoom, size, DS) {
+    size = size || 40; DS = DS || 9;
+    var HALF_MERC = 20037508.34;
+    var MAX_RESOLUTION = 156543.033906;
+    var resolution = MAX_RESOLUTION / Math.pow(2, zoom);    
+    var level = zoom;
+
+    do {
+        var gridLen = HALF_MERC / Math.pow(2, level);
+        var imgLen = size * resolution;
+        var outer = gridLen * 2 / 3;
+        var inner = gridLen * 1 / 3;   
+        if (imgLen > outer)
+            --level;
+        if (imgLen < inner)
+            ++level;
+    } while (imgLen > outer || imgLen < inner)
+    
+    dScore(geoJSONs, level, extent);
+    var clustered = new Array();
+    for (var i = 0; i < geoJSONs.length; ++i) {
+        if (geoJSONs[i].properties.dScore[level+1] >= DS)
+            clustered.push(geoJSONs[i]);
+    }
+    return clustered;
+}
+
+/*
+  function that computes the distinctive score of the features
+  mapCluster is based on this dScore.
+ */
+function dScore(features, level, extent) {
+    ++level;
+    var xOffset = [       0,   MERC/3, 2*MERC/3,
+                   2*MERC/3,   MERC/3,        0,
+                          0,   MERC/3, 2*MERC/3];
+    var yOffset = [       0,        0,        0,
+                     MERC/3,   MERC/3,   MERC/3,
+                   2*MERC/3, 2*MERC/3, 2*MERC/3];
+    for (var index = 0; index < 9; ++index) {
+        var left = extent[0] + xOffset[index];
+        var bottom = extent[1] + yOffset[index];
+        var right = extent[2] + xOffset[index];
+        var top = extent[3] + yOffset[index];
+        var gridLen = MERC/(Math.pow(2,level));
+        // x, y are grid coords
+        var x0 = Math.floor((left+HALF_MERC)/gridLen) - 1;
+        var x1 = Math.floor((right+HALF_MERC)/gridLen) + 1;
+        var y0 = Math.floor((bottom+HALF_MERC)/gridLen) - 1;
+        var y1 = Math.floor((top+HALF_MERC)/gridLen) + 1;
+        var vipArr = new Array(x1-x0+1);
+        for (var i = 0; i < vipArr.length; ++i) {
+            vipArr[i] = new Array(y1-y0+1);
+            for (var j = 0; j < vipArr[i].length; ++j) {
+                vipArr[i][j] = -1;
+            }
+        }
+        for (var i = 0; i < features.length; ++i) {
+            var x = 0, y = 0;
+            var xt, yt;
+            for (var j = 0; j <= level; ++j) {
+                switch (features[i].properties.mCode[index][j]) {
+                    case 0: x += Math.pow(2, level-j)*0;
+                            y += Math.pow(2, level-j)*0; break;
+                    case 1: x += Math.pow(2, level-j)*1;
+                            y += Math.pow(2, level-j)*0; break;
+                    case 2: x += Math.pow(2, level-j)*0;
+                            y += Math.pow(2, level-j)*1; break;
+                    case 3: x += Math.pow(2, level-j)*1;
+                            y += Math.pow(2, level-j)*1; break;
+                }
+            }
+            xt = x - x0;
+            yt = y - y0;
+            var feature = new ol.format.GeoJSON().readFeature(features[i], {
+                dataProjection: 'EPSG:4326',
+                featureProjection: 'EPSG:900913'
+            });
+            var coord = feature.getGeometry().getCoordinates();
+            if (features[i].properties.impScore > vipArr[xt][yt]) 
+                vipArr[xt][yt] = features[i];
+        }
+        for (var i = 0; i < vipArr.length; ++i) {
+            for (var j = 0; j < vipArr[i].length; ++j) {
+                if (vipArr[i][j] != -1) 
+                    ++vipArr[i][j].properties.dScore[level];
+            }
+        }
+    }
+}
+
